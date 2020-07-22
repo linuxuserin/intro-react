fa65853aedb73fadda519285e979032b8e8424f5
Injamul <unknownuserid2018@gmail.com>
2020-07-22 05:58:47 +0000

    Updates

---
+++ precache-manifest.3615769e19cd6aabbd52fb4c808af6ce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7211e40ec51dab9b542d7a0e85063e88",
+    "url": "/index.html"
+  },
+  {
+    "revision": "266dfa7293b4eac50374",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "b3feee2f19a5bc96b0e2",
+    "url": "/static/js/2.6fa26fd0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6fa26fd0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "266dfa7293b4eac50374",
+    "url": "/static/js/main.a9aa5a0e.chunk.js"
+  },
+  {
+    "revision": "55edaa45f478344ca7dd",
+    "url": "/static/js/runtime-main.90676495.js"
+  }
+]);
